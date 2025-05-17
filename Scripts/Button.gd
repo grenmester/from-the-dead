730baecc9daf3fa44d4b4d2acd47af2acb37e775
Jacky Lee <dr.jackylee@gmail.com>
2025-05-17 00:28:06 -0700

    feat: add colored doors and buttons

--- Scripts/Button.gd
+++ Scripts/Button.gd
@@ -1,29 +1,46 @@
 extends Area2D
 
-signal toggle(id)
+signal toggle(color)
 
-@export var id: int
+@export_enum("red", "green", "yellow") var color = "red"
 var toggled = false
+var disabledButton
+var enabledButton
 var body_count = 0
+
+func _ready():
+	configure()
+	disabledButton.show()
+
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("button_pushers"):
 		body_count += 1
 		if !toggled:
-			on()
+			disabledButton.hide()
+			enabledButton.show()
+			emit_signal("toggle", color)
 			toggled = true
+
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group("button_pushers"):
 		body_count -= 1
 		if body_count == 0 and toggled:
-			off()
+			enabledButton.hide()
+			disabledButton.show()
+			emit_signal("toggle", color)
 			toggled = false
 
-func on():
-	$AnimatedSprite2D.play("red")
-	emit_signal("toggle", id)
 
-func off():
-	$AnimatedSprite2D.stop()
-	emit_signal("toggle", id)
+func configure():
+	match color:
+		"red":
+			disabledButton = $DisabledRedButton
+			enabledButton = $EnabledRedButton
+		"green":
+			disabledButton = $DisabledGreenButton
+			enabledButton = $EnabledGreenButton
+		"yellow":
+			disabledButton = $DisabledYellowButton
+			enabledButton = $EnabledYellowButton
--- Scripts/Door.gd
+++ Scripts/Door.gd
@@ -1,21 +1,41 @@
 extends StaticBody2D
 
-@export var id: int
 @export var closed = true
+@export_enum("red", "green", "yellow") var color = "red"
+var openDoor
+var closedDoor
 
-func _ready() -> void:
-	if !closed:
-		configure()
+func _ready():
+	match color:
+		"red":
+			openDoor = $OpenRedDoor
+			closedDoor = $ClosedRedDoor
+		"green":
+			openDoor = $OpenGreenDoor
+			closedDoor = $ClosedGreenDoor
+		"yellow":
+			openDoor = $OpenYellowDoor
+			closedDoor = $ClosedYellowDoor
+
+	configure()
+
 	for button in get_tree().get_nodes_in_group("buttons"):
-		if button.id == id:
-			if !button.is_connected("toggle", toggle):
-				button.connect("toggle", toggle)
+		if button.color == color and !button.is_connected("toggle", toggle):
+			button.connect("toggle", toggle)
 
-func toggle(i):
-	if id == i:
+
+func toggle(button_color):
+	if button_color == color:
 		closed = !closed
 		configure()
 
+
 func configure():
-		$CollisionShape2D.set_deferred("disabled", !closed)
-		$Sprite2D.visible = closed
+	$CollisionShape2D.set_deferred("disabled", !closed)
+
+	if closed:
+		openDoor.hide()
+		closedDoor.show()
+	else:
+		openDoor.show()
+		closedDoor.hide()
