5ae18138ce8084f30a425033bac34feb8ff9b746
noedne <ney.brendan@gmail.com>
2025-05-18 04:16:26 -0700

    feat: add title screen

--- Scripts/MainScene.gd
+++ Scripts/MainScene.gd
@@ -1,7 +1,15 @@
 extends Control
 
+var started = false
+
 func _on_exit_button_pressed() -> void:
 	$LevelMenu.unload_level()
 
 func _on_reset_button_pressed() -> void:
 	$LevelMenu.reload_level()
+
+func _input(event) -> void:
+	if event.is_pressed() and !started:
+		$TitleScreen.hide()
+		$LevelMenu.show()
+		started = true
