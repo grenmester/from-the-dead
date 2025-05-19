a4ddaec800a9c62670f0f751afc664c8e40322da
noedne <ney.brendan@gmail.com>
2025-05-19 03:25:36 -0700

    fix: swordfish attack when flipped

--- Scripts/Swordfish.gd
+++ Scripts/Swordfish.gd
@@ -3,17 +3,23 @@ extends CharacterBody2D
 @export var speed = 150
 @export var flipped = false
 @onready var sprite = $Sprite2D
+@onready var tip = $Tip
 var corpse_scene = preload("res://Scenes/Objects/Corpse.tscn")
 
 func _ready() -> void:
+	if flipped:
+		configure()
+
+func configure() -> void:
 	sprite.flip_h = flipped
+	tip.scale.x = -1 if flipped else 1
 
 func _physics_process(delta: float) -> void:
 	velocity.x = speed if flipped else -speed
 	move_and_slide()
 	if is_on_wall():
 		flipped = !flipped
-		sprite.flip_h = flipped
+		configure()
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	if area.is_in_group("spines"):
