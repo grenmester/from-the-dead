87eb4254993124d41cc90a5dc78110670d2ac794
noedne <ney.brendan@gmail.com>
2025-05-18 22:43:35 -0700

    feat: make crates hangable

--- Scripts/Crate.gd
+++ Scripts/Crate.gd
@@ -1,9 +1,15 @@
 extends RigidBody2D
 
+@export var rope_id = -1
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	if rope_id != -1:
+		for rope in get_tree().get_nodes_in_group("ropes"):
+			if rope.id == rope_id and !rope.is_connected("snap", drop):
+				rope.connect("snap", drop)
+				freeze = true
+				set_collision_layer_value(1, true)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,6 +17,9 @@ func _process(delta: float) -> void:
 	pass
 
 
+func drop() -> void:
+	freeze = false
+	set_collision_layer_value(1, false)
 
 
 
--- Scripts/Mine.gd
+++ Scripts/Mine.gd
@@ -1,14 +1,15 @@
 extends CharacterBody2D
 
-@export var rope_id: int
+@export var rope_id = -1
 @export var gravity = 150
 var has_gravity = true
 
 func _ready() -> void:
-	for rope in get_tree().get_nodes_in_group("ropes"):
-		if rope.id == rope_id and !rope.is_connected("snap", drop):
-			rope.connect("snap", drop)
-			has_gravity = false
+	if rope_id != -1:
+		for rope in get_tree().get_nodes_in_group("ropes"):
+			if rope.id == rope_id and !rope.is_connected("snap", drop):
+				rope.connect("snap", drop)
+				has_gravity = false
 
 func _physics_process(delta: float) -> void:
 	if has_gravity:
