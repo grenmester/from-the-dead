41f592d731af64cfcb2100c874aba690e454263a
noedne <ney.brendan@gmail.com>
2025-05-20 05:48:04 -0700

    feat: add where's my shell popup

--- Scripts/Finish.gd
+++ Scripts/Finish.gd
@@ -3,5 +3,14 @@ extends Area2D
 signal win
 
 func _on_body_entered(body: Node2D) -> void:
-	if body.name == "Player" and body.shell_type == Enums.ShellType.NORMAL:
-		emit_signal("win")
+	if body.name == "Player":
+		if body.shell_type == Enums.ShellType.NORMAL:
+			emit_signal("win")
+		elif $Label.visible == false:
+			$Label.show()
+			$Timer.wait_time = 5
+			$Timer.start()
+
+
+func _on_timer_timeout() -> void:
+	$Label.hide()
